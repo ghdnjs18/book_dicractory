d843b5b92712e94f40935d75b7c82153a22fb903
ghdnjs18 <ghdnjs18@naver.com>
2021-11-01 16:14:23 +0900

    changed mongo db remote rul

--- src/routes/word/index.js
+++ src/routes/word/index.js
@@ -4,10 +4,11 @@ const Word = require('../../models/Word')
 
 // 정규표현식 ()? 옵션주기
 // wordRouter.route('/(:wordClass)?/(:word)?').get( async (req, res) => {
-wordRouter.route('/(:wordClass)?/(:word)?').get( async (req, res) => {
+wordRouter.route('/(:word)?').get( async (req, res) => {
     let words = []
     // 비구조화 할당 / 특정 property를 풀어서 가져올수있디.
-    const { wordClass, word } = req.params
+    // const { wordClass, word } = req.params
+    const { word } = req.params
     // console.log(word)
     
     if (word != "undefined" && word != undefined){
@@ -16,7 +17,7 @@ wordRouter.route('/(:wordClass)?/(:word)?').get( async (req, res) => {
         // DB에서 쿼리로 단어를 검색
         // 단어 명으로 찾기
         try {
-            if (wordClass === "wordName") {
+            // if (wordClass === "wordName") {
                 // 우리꺼
                 // words = await Word.find({keyword: {'$regex':word}});
                 // 연습중 ''로 안 묶어도 되네? 
@@ -40,10 +41,10 @@ wordRouter.route('/(:wordClass)?/(:word)?').get( async (req, res) => {
                     // 개수제한
                     // .limit(5);
 
-            } else {
+            // } else {
                 // 품사로 찾기
-                words = await Word.find({word_class: {'$regex':word}});
-            }
+                // words = await Word.find({word_class: {'$regex':word}});
+            // }
         } catch(e) {
             console.log(e)
         }
