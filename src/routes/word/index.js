83e1cb0aec34b7055ffae4529647b846931d437d
ghdnjs18 <ghdnjs18@naver.com>
2021-11-01 15:52:06 +0900

    database conected

--- src/routes/word/index.js
+++ src/routes/word/index.js
@@ -10,119 +10,50 @@ wordRouter.route('/(:wordClass)?/(:word)?').get( async (req, res) => {
     // console.log(word)
     
     if (word != "undefined" && word != undefined){
-        // const queries = word.replace(/,/gi, '|')
+        const queries = word.replace(/,/gi, '|')
         // console.log(queries)
         // DB에서 쿼리로 단어를 검색
         // 단어 명으로 찾기
         try {
-            words = [ 
-                { 
-                    seq: "1",
-                    keyword: "학원", 
-                    link: "https//google.com", 
-                    hanja: "한자", 
-                    word_class: "포스", 
-                    meaning: "학원은 지루하다", 
-                }, 
-                 { 
-                    seq: "1",
-                    keyword: "학원", 
-                    link: "https//google.com",
-                    hanja: "한자", 
-                    word_class: "포스", 
-                    meaning: "학원은 지루하다", 
-                } 
-            ]
-
-            // if (wordClass === "wordName") {
-            //     // 우리꺼
-            //     // words = await Word.find({keyword: {'$regex':word}});
-            //     // 연습중 ''로 안 묶어도 되네? 
-            //     // words = await Word.find({keyword: {'$regex':`^${word}`}});
-            //     // words = await Word.find({keyword: {'$regex':`${word}$`}});
-            //     // words = await Word.find({keyword: {$regex: word}});
+            if (wordClass === "wordName") {
+                // 우리꺼
+                // words = await Word.find({keyword: {'$regex':word}});
+                // 연습중 ''로 안 묶어도 되네? 
+                // words = await Word.find({keyword: {'$regex':`^${word}`}});
+                // words = await Word.find({keyword: {'$regex':`${word}$`}});
+                // words = await Word.find({keyword: {$regex: word}});
                 
-            //     words = await Word.find({
-            //         $or: [
-            //             {keyword: {$regex: queries}},
-            //             {meaning: {$regex: queries}}
-            //         ]});
+                words = await Word.find({
+                    $or: [
+                        {keyword: {$regex: queries}},
+                        {meaning: {$regex: queries}}
+                    ]});
 
-            //     // words = await Word.find({
-            //     //     $or: [
-            //     //         {keyword: {$regex: word}},
-            //     //         {meaning: {$regex: word}}
-            //     //     ]
-            //     //     // 오림차순 1 asc / 내림차순 -1 desc
-            //     //     }).sort({'_id': 'asc'})
-            //     //     // 개수제한
-            //     //     // .limit(5);
+                // words = await Word.find({
+                //     $or: [
+                //         {keyword: {$regex: word}},
+                //         {meaning: {$regex: word}}
+                //     ]
+                //     // 오림차순 1 asc / 내림차순 -1 desc
+                //     }).sort({'_id': 'asc'})
+                //     // 개수제한
+                //     // .limit(5);
 
-            // } else {
-            //     // 품사로 찾기
-            //     words = await Word.find({word_class: {'$regex':word}});
-            // }
+            } else {
+                // 품사로 찾기
+                words = await Word.find({word_class: {'$regex':word}});
+            }
         } catch(e) {
             console.log(e)
         }
     }else {
         // console.log(word)
         try {
-            words = [ 
-                { 
-                    seq: "1",
-                    keyword: "학원", 
-                    link: "https//google.com", 
-                    hanja: "한자", 
-                    word_class: "포스", 
-                    meaning: "학원은 지루하다", 
-                }, 
-                 { 
-                    seq: "1",
-                    keyword: "학원", 
-                    link: "https//google.com",
-                    hanja: "한자", 
-                    word_class: "포스", 
-                    meaning: "학원은 지루하다", 
-                }, 
-                 { 
-                    seq: "1",
-                    keyword: "학원", 
-                    link: "https//google.com",
-                    hanja: "한자", 
-                    word_class: "포스", 
-                    meaning: "학원은 지루하다", 
-                }, 
-                 { 
-                    seq: "1",
-                    keyword: "학원", 
-                    link: "https//google.com",
-                    hanja: "한자", 
-                    word_class: "포스", 
-                    meaning: "학원은 지루하다", 
-                }, 
-                 { 
-                    seq: "1",
-                    keyword: "학원", 
-                    link: "https//google.com",
-                    hanja: "한자", 
-                    word_class: "포스", 
-                    meaning: "학원은 지루하다", 
-                }, 
-                 { 
-                    seq: "1",
-                    keyword: "학원", 
-                    link: "https//google.com",
-                    hanja: "한자", 
-                    word_class: "포스", 
-                    meaning: "학원은 지루하다", 
-                }
-            ]
-            // words = await Word.find()
+            // DB에서 전체 단어 검색
+            words = await Word.find()
         } catch(e) {
             console.log(e)
         }
-        // DB에서 전체 단어 검색
     }
     res.json({status:200, words})
 })
