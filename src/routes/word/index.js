dc28bc59c939dc85c8b9aadb4de6d619433b7a6a
ghdnjs18 <ghdnjs18@naver.com>
2021-11-01 16:06:20 +0900

    changed mongo db remote rul

--- src/routes/word/index.js
+++ src/routes/word/index.js
@@ -3,6 +3,7 @@ const wordRouter = express.Router()
 const Word = require('../../models/Word')
 
 // 정규표현식 ()? 옵션주기
+// wordRouter.route('/(:wordClass)?/(:word)?').get( async (req, res) => {
 wordRouter.route('/(:wordClass)?/(:word)?').get( async (req, res) => {
     let words = []
     // 비구조화 할당 / 특정 property를 풀어서 가져올수있디.
@@ -10,7 +11,7 @@ wordRouter.route('/(:wordClass)?/(:word)?').get( async (req, res) => {
     // console.log(word)
     
     if (word != "undefined" && word != undefined){
-        const queries = word.replace(/,/gi, '|')
+        // const queries = word.replace(/,/gi, '|')
         // console.log(queries)
         // DB에서 쿼리로 단어를 검색
         // 단어 명으로 찾기
@@ -23,21 +24,21 @@ wordRouter.route('/(:wordClass)?/(:word)?').get( async (req, res) => {
                 // words = await Word.find({keyword: {'$regex':`${word}$`}});
                 // words = await Word.find({keyword: {$regex: word}});
                 
-                words = await Word.find({
-                    $or: [
-                        {keyword: {$regex: queries}},
-                        {meaning: {$regex: queries}}
-                    ]});
-
                 // words = await Word.find({
                 //     $or: [
-                //         {keyword: {$regex: word}},
-                //         {meaning: {$regex: word}}
-                //     ]
-                //     // 오림차순 1 asc / 내림차순 -1 desc
-                //     }).sort({'_id': 'asc'})
-                //     // 개수제한
-                //     // .limit(5);
+                //         {keyword: {$regex: queries}},
+                //         {meaning: {$regex: queries}}
+                //     ]});
+
+                words = await Word.find({
+                    $or: [
+                        {keyword: {$regex: word}},
+                        {meaning: {$regex: word}}
+                    ]
+                    // 오림차순 1 asc / 내림차순 -1 desc
+                    }).sort({'_id': 'asc'})
+                    // 개수제한
+                    // .limit(5);
 
             } else {
                 // 품사로 찾기
